191ab63dcc14e8531873e5ac62e005ff1c83dc33
Abu Bakar Aakif Muhammad <95681384+Aakif52@users.noreply.github.com>
2022-04-03 17:25:39 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 main()
-int nasiPadang;
-int NasiKedua;
+	int array[70];
+	int i, num, a = 75, b = 100, j;
+	int SIZE = sizeof(array)/sizeof(array[0]);
+	
+	//inisialisasi 70 nilai array secara random dari 75--100
+	srand(time(0));
+	
+	for( i = 0; i < SIZE; i++){
+		num = (rand() % (b - a + 1)) + a;
+		array[i] = num;
+		printf(" %d ", array[i]);
+	}
+}
