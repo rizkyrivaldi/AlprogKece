8aa5d0458bc43a673e4745b3bffe75fc9e669ee9
Abu Bakar Aakif Muhammad <95681384+Aakif52@users.noreply.github.com>
2022-04-04 14:05:11 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#indlude <time.h>
+#include <time.h>
 #include "statistik.h"
 
 int main(){
-	int array[70];
-	int i, num, a = 75, b = 100, j;
-	int SIZE = sizeof(array)/sizeof(array[0]);
+	int KUIS[30], UTS[30], UAS[30];
+	int i, num, num1, num2, a = 75, b = 100, j;
+	int SIZE = sizeof(KUIS)/sizeof(KUIS[0]);
 	
 	//inisialisasi 70 nilai array secara random dari 75--100
 	srand(time(0));
 	
+	printf(" No. Absen\t||\t Nilai KUIS\t||\t Nilai UTS\t||\t Nilai UAS\t||\n");
+	
 	for( i = 0; i < SIZE; i++){
 		num = (rand() % (b - a + 1)) + a;
-		array[i] = num;
-		printf(" %d ", array[i]);
+		num1 = (rand() % (b - a + 1)) + a;
+		num2 = (rand() % (b - a + 1)) + a;
+		KUIS[i] = num;
+		UTS[i] = num1;
+		UAS[i] = num2;
+		
+		printf("\t%d\t||\t  %d\t\t||\t  %d\t\t||\t  %d\t\t||\n", i+1, KUIS[i], UTS[i], UAS[i]);
 	}
+	
+	
 }
